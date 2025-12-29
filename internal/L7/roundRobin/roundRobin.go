aaa655ecfdbd4d584fc464bafb1cdfdb9d09b6f8
DKMFzF <flim.win@yandex.ru>
2025-12-29 20:46:39 +0700

    feat(roundRobine): added main functions

--- internal/L7/roundRobin/roundRobin.go
+++ internal/L7/roundRobin/roundRobin.go
@@ -1,1 +1,16 @@
 package roundrobin
+
+import (
+	"net/url"
+	"sync/atomic"
+)
+
+type RoundRobineBalancer struct {
+	backends []*url.URL
+	counter  uint64
+}
+
+func (rr *RoundRobineBalancer) NextBackend() *url.URL {
+	index := atomic.AddUint64(&rr.counter, 1)
+	return rr.backends[index%uint64(len(rr.backends))]
+}
